44aeb3683750ae448b25c06c5fe7c303848c4d7b
Sowayi <sapwao@gmail.com>
2019-05-19 13:43:25 -0400

    Fix spacing errors in class Everything __init__ method

--- app/models/everything.py
+++ app/models/everything.py
@@ -3,14 +3,14 @@ class Everything:
     Class Everything that acts as a blueprint for creating 
     instances of unfiltered news
     """
-    def __init__(self, author, title, description, url, urlToImage, publishedAt)
+
+    def __init__(self, author, title, description, url, urlToImage, publishedAt):
         """
         __init__ method to initialize attributes of the class Everything
         """
         self.author = author
         self.title = title
         self.description = description
-        self. url = url 
+        self.url = url
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
-        
