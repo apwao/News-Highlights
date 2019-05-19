3b378e98ec405927152331886629921fc1c3f30a
Sowayi <sapwao@gmail.com>
2019-05-19 05:10:56 -0400

    Add class sources to model data received from sources api

---
+++ app/models/topheadlines.py
@@ -0,0 +1,10 @@
+class TopHeadlines:
+    """
+    Class TopHeadlines that acts as a blueprint for creating instances of
+    headlines from a given source
+    """
+
+    def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt, content)
+        """
+        
+        """
