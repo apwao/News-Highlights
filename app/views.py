3f3e287e17f34c259b36542aada7b79e580beb27
Sowayi <sapwao@gmail.com>
2019-05-19 10:13:26 -0400

    Add news sources to the default route of the page

--- app/views.py
+++ app/views.py
@@ -6,10 +6,21 @@ def index():
     """
     A function than returns the home page when called upon
     """
+    #get all available news sources
+    news_sources = get_sources()
+    
+    #get all news articles available
+    everything = get_everything()
 
     title = 'Home - Find all the current news at your convinience'
 
-    return render_template('index.html' title = title)
+    return render_template('index.html' title = title, new_sources = news_sources, everything = everything)
+
+@app.route('/news/<source>')
+def topheadlines(source):
+    """
+    A function that returns the 
+    """
 
 
 
