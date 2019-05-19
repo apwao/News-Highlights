3984f582a717877c0752906c300aa6857957d696
Sowayi <sapwao@gmail.com>
2019-05-19 05:17:47 -0400

    Add class TopHeadlines to model headlines data from api

--- app/models/topheadlines.py
+++ app/models/topheadlines.py
@@ -4,7 +4,19 @@ class TopHeadlines:
     headlines from a given source
     """
 
-    def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt, content)
+    def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt)
         """
+        __init__ method to initialize parameters required to create 
+        instances of class TopHeadlines
+        """
+
+        self.id = id
+        self.name = name 
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url 
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
         
-        """
+
