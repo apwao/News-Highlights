39590a6ef07b62ebe3352091aefc075fef6e557c
Sowayi <sapwao@gmail.com>
2019-05-19 13:33:20 -0400

    Change everything_base_url to display alternative news

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config:
     """
     ALL_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?q=trending&apiKey={}'
+    EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?q=fashion&apiKey={}'
 
 class ProdConfig(Config):
     """
