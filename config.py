2e029c193f0e4cef9c0da8c00c1a2ff4b9ddda13
Sowayi <sapwao@gmail.com>
2019-05-20 06:30:55 -0400

    deployment to heroku

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     The main configurations parent class with configurations common to both 
     development and production
     """
-    NEWS_API_KEY = os.environ.get('f3bfbf8ef8044c9fb41635b68edb0360')
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     ALL_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?q=fashion&apiKey={}'
